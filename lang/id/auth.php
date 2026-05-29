39376bffa4449aaa63bbc1ed3faafae6b64228bb
syam-shoot <syam-shoot@users.noreply.github.com>
2026-05-29 18:36:31 +0800

    feat: Indonesian language for auth error messages

---
+++ lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Email atau kata sandi salah.',
+    'password' => 'Kata sandi yang dimasukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+];
